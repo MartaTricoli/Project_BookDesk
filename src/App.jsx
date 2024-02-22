e0aedb55ee6a116d3ef8809567f2fbc43995a2f2
Emilio <lemigliot@gmail.com>
2024-02-22 18:40:28 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import Sidebar from "./components/shared/Sidebar";
+import MySelf from "./pages/Profile";
+import Tabs from "./pages/Library";
+
 const App = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
     <>
+      <div className="flex flex-col">
+        <div className="min-w-[1080px]:ml-[138px] ml-[65px] h-full">
+          <MySelf />
+        </div>
+        <div className="flex justify-center mt-10">
+          <Tabs />
+        </div>
+        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,32 @@
+const MySelf = () => {
+
+
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center w-full h-auto filter drop-shadow-lg">
+                <div className="relative top-10 w-[fit-content] sm:w-1/2 p-8 mx-auto border-2 border-white rounded-3xl overflow-auto bg-new_beige">
+                    <h1 className="text-4xl lg:text-8xl">Ben tornato</h1>
+
+                    <div className="flex items-center justify-between">
+
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col items-center justify-center w-full h-auto filter drop-shadow-lg ">
+
+                <div className="relative top-10 w-1/2 pt-8  mx-auto border-2 border-white rounded-3xl overflow-auto bg-new_beige">
+                    <h1 className=" text-4xl lg:text-8xl">Maccio Capatonda</h1>
+                    <div className="flex items-center justify-center">
+                        <img
+                            src="https://pbs.twimg.com/profile_images/1169515344232419328/deUK52kE_400x400.jpg"
+                            className="w-32 rounded-lg shadow-lg"
+                            alt="Avatar" />
+                    </div>
+                    <h2 className="text-4xl lg:text-8xl w-4/5 mt-8 mb-0 mx-auto border-2 border-white border-opacity-50 rounded-full p-10">Marcello Macchia</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MySelf
