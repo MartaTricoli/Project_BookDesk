84d32b7055c569a8b5ddd73dbf057a2bc394adf3
Emilio <lemigliot@gmail.com>
2024-02-26 17:25:29 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Sidebar from "./components/shared/Sidebar";
 import MySelf from "./pages/Profile";
-import Tabs from "./pages/Library";
 
 const App = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -16,11 +15,10 @@ const App = () => {
         <div className="min-w-[1080px]:ml-[138px] ml-[65px] h-full">
           <MySelf />
         </div>
-        <div className="flex justify-center mt-10">
-          <Tabs />
-        </div>
+
         <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       </div>
+      
     </>
   );
 };
