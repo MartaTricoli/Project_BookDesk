8f62bd379b7557821c5bcf75788c7b2181af1505
Emilio <lemigliot@gmail.com>
2024-03-22 18:31:35 +0100

    fixed

--- src/components/shared/ActualBook.jsx
+++ src/components/shared/ActualBook.jsx
@@ -1,5 +1,3 @@
-import React, { useState } from 'react';
-
 const ActualBook = () => {
     const [progressValue, setProgressValue] = useState(33);
     const [inputValue, setInputValue] = useState("");
@@ -50,52 +48,6 @@ const ActualBook = () => {
                             </button>
                         </div>
                     </div>
-                </div>
-                <div>
-
-
-
-                    <div className="p-6">
-                        <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-white">
-                            Titolo Libro Bello
-                        </h5>
-                    </div>
-                    <ul className="w-full">
-                        <li className="w-full px-6 py-3 dark:border-opacity-50 dark:text-white ">
-                            Autore:
-                        </li>
-                        <li className="w-full px-6 py-3 dark:border-opacity-50 dark:text-white">
-                            Categoria:
-                        </li>
-                        <li className="w-full px-6 py-3 dark:border-opacity-50 dark:text-white">
-                            Anno di pubblicazione:
-                        </li>
-
-                    </ul>
-                    <div className="p-6 flex flex-row max-sm:flex-col gap-8">
-                        <button className="relative inline-block text-lg group">
-                            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                                <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                                <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-new_light_blue group-hover:-rotate-180 ease"></span>
-                                <span className="relative">Info Libro</span>
-                            </span>
-                            <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
-                        </button>
-                        <button className="relative inline-block text-lg group">
-                            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                                <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                                <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-new_light_blue group-hover:-rotate-180 ease"></span>
-                                <span className="relative">Mercato</span>
-                            </span>
-                            <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
-                        </button>
-                    </div>
-                    <ul>
-
-                        <li className="w-full px-6 py-3 dark:border-opacity-50 dark:text-white">
-                            Pagine totali
-                        </li>
-                    </ul>
                 </div>
 
             </div>
--- src/pages/AllMyBooks.jsx
+++ src/pages/AllMyBooks.jsx
@@ -10,6 +10,9 @@ const AllMyBooks = () => {
     const [books, setBooks] = useState([]);
     const token = useSelector((state) => state.auth.token);
     const user = useSelector((state) => state.auth.user);
+    const [selectedBookStatuses, setSelectedBookStatuses] = useState({});
+    const [selectedBookPages, setSelectedBookPages] = useState({});
+    const [modifiedBookPages, setModifiedBookPages] = useState({});
 
     const fetchAllBooks = async () => {
         setError(false);
@@ -24,7 +27,6 @@ const AllMyBooks = () => {
             });
 
             setAllBook(result.data.docs);
-            console.log("primo step", allBook);
         } catch (error) {
             setError(error);
         }
@@ -32,7 +34,7 @@ const AllMyBooks = () => {
     }
 
     const refetchAllBooks = () => {
-        allBook.map(async book => {
+        allBook.forEach(async book => {
             if (book.user === user._id) {
                 try {
                     const result = await axios({
@@ -41,21 +43,61 @@ const AllMyBooks = () => {
                         headers: {
                             Authorization: `Bearer ${token}`
                         }
-                    })
-                   
-                    setBooks(book => [...book, result.data]);
+                    });
+
+                    setBooks(prevBooks => [...prevBooks, result.data]);
+                    setSelectedBookStatuses(prevState => ({
+                        ...prevState,
+                        [book._id]: result.data.status || ""
+                    }));
+                    setSelectedBookPages(prevPages => ({
+                        ...prevPages,
+                        [book._id]: result.data.info.pages || 0
+                    }));
                 } catch (error) {
                     console.log(error);
                 }
             }
         });
     }
-    
+
     useEffect(() => {
         fetchAllBooks();
     }, []);
 
-    return(
+    useEffect(() => {
+        const storedBookStatuses = JSON.parse(localStorage.getItem("selectedBookStatuses"));
+        if (storedBookStatuses) {
+            setSelectedBookStatuses(storedBookStatuses);
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("selectedBookStatuses", JSON.stringify(selectedBookStatuses));
+    }, [selectedBookStatuses]);
+
+
+
+    const handleBookStatusChange = (bookId, status) => {
+        setSelectedBookStatuses(prevState => ({
+            ...prevState,
+            [bookId]: status
+        }));
+    };
+
+    const handleBookPagesChange = (bookId, pages) => {
+        setSelectedBookPages(prevPages => ({
+            ...prevPages,
+            [bookId]: pages
+        }));
+
+        setModifiedBookPages(prevPages => ({
+            ...prevPages,
+            [bookId]: pages
+        }));
+    };
+
+    return (
         <div className="flex flex-col items-center mt-20 dark:bg-new_navy_blue h-full">
             <h3 className="text-5xl font-bold text-new_navy_blue dark:text-white w-[1480px] text-center ml-40 mb-8 mt-4">Your books</h3>
             <div className="flex gap-12 mb-8">
@@ -68,22 +110,47 @@ const AllMyBooks = () => {
             </div>
             <div className="flex flex-wrap justify-start w-[1480px] ml-52 mb-96">
                 {error && <p className="pl-52">There was an error, books not available.</p>}
-                {loading && allBook.length === 0 && 
+                {loading && allBook.length === 0 &&
                     <div>
                         <p className="pl-52">Loading...</p>
                         <p className="h-screen"></p>
                     </div>
                 }
-                {!loading && !error && allBook.length === 0 && <p className="pl-52">You have not add any book yet.</p>}
+                {!loading && !error && allBook.length === 0 && <p className="pl-52">You have not added any book yet.</p>}
                 {!loading && !error && books.length === 0 && <p className="h-screen"></p>}
                 {books.length > 0 && books.map(book => (
                     <div key={book._id}>
-                        <div className=" bg-white p-8 rounded-lg shadow-sm hover:shadow-lg hover:shadow-new_pastel_blue shadow-new_pastel_blue max-w-md m-4">
+                        <div className="bg-white p-8 rounded-lg shadow-sm hover:shadow-lg hover:shadow-new_pastel_blue shadow-new_pastel_blue max-w-md m-4">
                             <div className="mb-4 flex justify-center">
-			                    <img src={book.cover} alt="Post Image" className="h-48 object-cover rounded-md cursor-pointer" />
+                                <img src={book.cover} alt="Post Image" className="h-48 object-cover rounded-md cursor-pointer" />
                                 <div className="text-new_navy_blue pl-4 pt-2">
                                     <h3 className="font-bold">{book.name}</h3>
                                     <h2 className="font-medium">{book.author}</h2>
+                                    <h2 className="font-medium">Total Pages: {book.info.pages}</h2>
+                                    <input
+                                        type="number"
+                                        value={modifiedBookPages[book._id] || ""}
+                                        onChange={(e) => {
+                                            const newValue = parseInt(e.target.value);
+                                            if (!isNaN(newValue) && newValue >= 0 && newValue <= book.info.pages) {
+                                                handleBookPagesChange(book._id, newValue);
+                                            }
+                                        }}
+                                        className="border border-gray-300 rounded-md px-2 py-1 mt-2"
+                                        placeholder="Page"
+                                        min="0"
+                                        max={book.info.pages}
+                                    />
+                                    <select
+                                        value={selectedBookStatuses[book._id] || ""}
+                                        onChange={(e) => handleBookStatusChange(book._id, e.target.value)}
+                                        className="border border-gray-300 rounded-md px-2 py-1 mt-2"
+                                    >
+                                        <option value="" disabled>Select status</option>
+                                        <option value="read">Read</option>
+                                        <option value="reading">Reading</option>
+                                        <option value="to read">To Read</option>
+                                    </select>
                                 </div>
                             </div>
                         </div>
@@ -91,7 +158,7 @@ const AllMyBooks = () => {
                 ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default AllMyBooks;
